594c16e594bcb608b6fd5821d1e6d633e35a6a5c
jose-luis-hernandez-claros <joluhernandezc@correo.udistrital.edu.co>
2021-03-10 00:44:03 -0500

    1

--- app/src/main/java/com/example/help_pets/MainActivity.java
+++ app/src/main/java/com/example/help_pets/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
 
     }
 }
