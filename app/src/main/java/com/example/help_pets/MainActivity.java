84b6759bbe0f874d5f3ead576af3700314640753
jose-luis-hernandez-claros <joluhernandezc@correo.udistrital.edu.co>
2021-04-05 16:58:38 -0500

    1

--- app/src/main/java/com/example/help_pets/MainActivity.java
+++ app/src/main/java/com/example/help_pets/MainActivity.java
@@ -17,12 +17,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    //intent para abrir pestaña mascotas
     public void mascotas(View view){
         Intent mascotas = new Intent(this, com.example.help_pets.mascotas.class);
         startActivity(mascotas);
     }
+
+    //Intent para abrir pestaña maps
     public void maps(View view){
         Intent maps = new Intent(this, ubicacionveterinarias.class);
         startActivity(maps);
     }
+
+    //intent para abrir pestaña dietas recomendadas
+    public void dietas(View view){
+        Intent dietas = new Intent(this, com.example.help_pets.dietas.class);
+        startActivity(dietas);
+    }
+
+    //intent para abrir pestaña registro mascotas
+    public  void registromascotas(View view){
+        Intent registro = new Intent(this,com.example.help_pets.registromascotas.class);
+        startActivity(registro);
+    }
+
+    //intent para abrir pestaña registro de vacunas
+    public void registrovacunas(View view){
+        Intent rvacunas = new Intent(this,com.example.help_pets.vacunas.class);
+        startActivity(rvacunas);
+    }
+
 }
