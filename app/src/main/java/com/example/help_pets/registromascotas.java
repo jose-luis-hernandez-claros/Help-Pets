3c73b4ace53afb5202fc6deb7b2caeca8b64e4d3
jose-luis-hernandez-claros <joluhernandezc@correo.udistrital.edu.co>
2021-04-05 23:29:57 -0500

    1

--- app/src/main/java/com/example/help_pets/registromascotas.java
+++ app/src/main/java/com/example/help_pets/registromascotas.java
@@ -3,12 +3,35 @@ package com.example.help_pets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class registromascotas extends AppCompatActivity {
+
+
+    private Spinner tipomascota;
+    private  Spinner sexomascota;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registromascotas);
+
+        tipomascota = (Spinner)findViewById(R.id.spinnertipomascota1);
+        sexomascota = (Spinner)findViewById(R.id.spinnersexomascota1);
+
+        //Tipos de mascotas
+
+        String [] mascotas = {"Seleccionar...","Perro","Gato","Ave","Pez","Hamnster"};
+
+        ArrayAdapter <String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, mascotas);
+        tipomascota.setAdapter(adapter);
+
+
+        //Sexo de mascota
+        String [] sexomascota1 = {"Seleccionar...","Macho","Hembra"};
+        ArrayAdapter <String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, sexomascota1);
+        sexomascota.setAdapter(adapter2);
+
     }
 }
--- app/src/main/java/com/example/help_pets/vacunas.java
+++ app/src/main/java/com/example/help_pets/vacunas.java
@@ -3,12 +3,34 @@ package com.example.help_pets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class vacunas extends AppCompatActivity {
+
+    private Spinner tipomascota;
+    private Spinner sexomascota;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vacunas);
+
+        tipomascota = (Spinner)findViewById(R.id.spinnertipomascota2);
+        sexomascota = (Spinner)findViewById(R.id.spinnersexomascota2);
+
+
+        //Tipo de mascota
+
+        String [] mascotas = {"Seleccionar...","Perro","Gato","Ave","Pez","Hamnster"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, mascotas);
+        tipomascota.setAdapter(adapter);
+
+
+        //sexo de la mascota
+        String [] sexomascota1 = {"Seleccionar...","Macho","Hembra"};
+        ArrayAdapter <String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, sexomascota1);
+        sexomascota.setAdapter(adapter2);
     }
 }
