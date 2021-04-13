17c151c352c7e890dedf4293ccd16fc938de8aa6
jose-luis-hernandez-claros <joluhernandezc@correo.udistrital.edu.co>
2021-04-12 22:14:32 -0500

    1

--- app/src/main/java/com/example/help_pets/dietas.java
+++ app/src/main/java/com/example/help_pets/dietas.java
@@ -2,7 +2,9 @@ package com.example.help_pets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class dietas extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class dietas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dietas);
+    }
+
+    //abrir pestaña dietas de perros
+    public void dietaperros(View view){
+        Intent dietaperros = new Intent(this, com.example.help_pets.dietasperros.class);
+        startActivity(dietaperros);
     }
 }
